5f70e4ab01cf44af8604fc8ac2da6a312bda524b
danvarga1985 <danvarga1985@gmail.com>
2020-12-17 10:29:09 +0100

    Button onClick listeners

--- app/src/main/java/com/danvarga/weatherapiapp/MainActivity.java
+++ app/src/main/java/com/danvarga/weatherapiapp/MainActivity.java
@@ -1,13 +1,53 @@
 package com.danvarga.weatherapiapp;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn_cityID, btn_getWeatherByID, btn_getWeatherByName;
+    EditText et_dataInput;
+    ListView lv_weatherReport;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // Assign values to each control on the layout.
+        btn_cityID = findViewById(R.id.btn_getCityID);
+        btn_getWeatherByID = findViewById(R.id.btn_getWeatherByCityId);
+        btn_getWeatherByName = findViewById(R.id.btn_getWeatherByCityName);
+
+        et_dataInput = findViewById(R.id.et_DataInput);
+        lv_weatherReport = findViewById(R.id.lv_weatherReports);
+
+        // Click listeners for each button.
+        btn_getWeatherByID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "You clicked me - ID", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btn_getWeatherByName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "You typed " + et_dataInput.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btn_cityID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "You clicke me - CITYID", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
